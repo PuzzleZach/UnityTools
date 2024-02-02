e36e9d57826695c07b52fe653f2f6a40f9776dc8
Zach <132305891+PuzzleZach@users.noreply.github.com>
2024-02-02 16:33:16 -0600

    Create AtlasApply.cs

---
+++ AtlasApply.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using System.IO;
+
+public class AtlasApply : MonoBehaviour
+{
+    public Material newAtlasMaterial;
+
+    [SerializeField]
+    public string modelFolder;
+    
+    [SerializeField]
+    public string fileExtension = "*.fbx";
+
+    [ContextMenu("ApplyAtlas")]
+    void ApplyAtlasToModels()
+    {
+        string folderPath = Path.Combine(Application.dataPath, modelFolder);
+        string[] modelPaths = Directory.GetFiles(folderPath, fileExtension);
+
+        foreach (string modelPath in modelPaths)
+        {
+            string newPath = modelPath.Split("AssetJam")[1].Replace("\\", "/").Substring(1);
+            GameObject model =  AssetDatabase.LoadAssetAtPath<GameObject>(newPath);
+            
+            if (model == null) continue;
+
+            Renderer[] renderers = model.GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer renderer in renderers)
+            {
+                Material[] materials = renderer.sharedMaterials;
+
+                for (int i = 0; i < materials.Length; i++)
+                {
+                    if (materials[i].name == "Atlas")
+                    {
+                        materials[i] = newAtlasMaterial;   
+                    }
+                }
+                renderer.materials = materials;
+            }
+        }
+        AssetDatabase.Refresh();
+    }
+}
